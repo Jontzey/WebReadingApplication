36b21d3c626bb2a09b2cff44473c3054680c39ee
Jontzey <Jonathan.andersson1997@outlook.com>
2023-07-07 22:26:27 +0200

    last commit before bed
    added a background picture just because

--- src/RouterComponents/Home.jsx
+++ src/RouterComponents/Home.jsx
@@ -5,8 +5,8 @@ import ReactPlayer from 'react-player';
 
 
 function Home() {
-    return (<div style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
-        <h1>Welcome to a Manga Lovers project!</h1>
+    return (<div className='home' style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
+        <h1>Welcome to a Manga Lover project!</h1>
         <h2>This is just a project for me as a developer to learn more about coding and aswell to make design</h2>
         <h3>Feel free to explore my simple project and give me tips and more so I can grow and get experience</h3>
         <img style={{width:"400px", borderRadius:"20px"}} src="https://media1.giphy.com/media/fhAwk4DnqNgw8/giphy.gif?cid=ecf05e47glb0uq05bxc4qje8r3b18yaol39wotjuzit5ztnn&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt='Error, img not found'/>
@@ -16,8 +16,8 @@ function Home() {
             </h2> 
             <div style={{display:"flex", flexDirection:"column", justifyContent:"center", gap:"3px"}}>
                 <h3 style={{margin:"0"}}><li>Home =>
-                as you see this page has content with simple text</li></h3>
-                <h3 style={{margin:"0"}}><li>Mangas => pressing Mangas in header will navigate you to the url with current mangas and give you options to choose manga aswell chapters in it</li></h3>
+                As you see this page has content with simple text</li></h3>
+                <h3 style={{margin:"0"}}><li>Mangas => Pressing Mangas in header will navigate you to the url with current mangas and give you options to choose manga aswell chapters in it</li></h3>
                 <h3 style={{margin:"0"}}> <li>About => Not finished but there will be text about the project and as well what packages may have been downloaded</li></h3>
                
             </div>
